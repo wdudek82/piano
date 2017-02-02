f0bef4d09c4afddc8e821606e70dc426899127fc
ChiliG <chrisgray2017@gmail.com>
2017-02-02 09:23:45 -0800

    removed comments
    I might have been editing comments

--- js/piano.js
+++ js/piano.js
@@ -1,20 +1,4 @@
-// var c1 = document.getElementById("c1");
-// var d1 = document.getElementById("c1");
-// var e1 = document.getElementById("c1");
-// var f1 = document.getElementById("c1");
-// var g1 = document.getElementById("c1");
-// var a1 = document.getElementById("c1");
-// var h1 = document.getElementById("c1");
-// var c2 = document.getElementById("c1");
 
-// c1.addEventListener('keypress', playSound("piano_c1"));
-// d1.addEventListener('keypress', playSound("piano_d1"));
-// e1.addEventListener('click', playSound("piano_e1"));
-// f1.addEventListener('click', playSound("piano_f1"));
-// g1.addEventListener('click', playSound("piano_g1"));
-// a1.addEventListener('click', playSound("piano_a1"));
-// h1.addEventListener('click', playSound("piano_h1"));
-// c2.addEventListener('click', playSound("piano_c2"));
 
 function playSound(sound) {
 	console.log("Sound: "+sound.slice(-2));
